05a4cf5fc039d5566052ea8924de34f963e436c0
Julia157 <86552992+Julia157@users.noreply.github.com>
2023-05-22 13:49:18 +0300

    test

--- index.js
+++ index.js
@@ -5,8 +5,15 @@ logo = document.getElementById("logo").style;
 audio = document.getElementById("audio");
 
 window.onclick = function () {
-  preparation();
-  audio.play();
+  if (!context) {
+    preparation();
+  }
+  if (audio.paused) {
+    audio.play();
+    loop();
+  } else {
+    audio.pause();
+  }
 };
 
 function preparation() {
@@ -19,7 +26,9 @@ function preparation() {
 }
 
 function loop() {
-  window.requestAnimationFrame(loop);
+  if (!audio.paused) {
+    window.requestAnimationFrame(loop);
+  }
   array = new Uint8Array(analyser.frequencyBinCount);
   analyser.getByteFrequencyData(array);
 
