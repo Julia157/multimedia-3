857c6989a2509fcfed7b501d6595fd6a7f0ad1b1
Julia157 <86552992+Julia157@users.noreply.github.com>
2023-05-26 23:11:40 +0300

    final changes

--- index.js
+++ index.js
@@ -1,37 +1,108 @@
-var audio, context, analyser, src, array, logo;
+var slideIndex = 1;
+var startBtn = document.getElementById("startBtn");
+var container = document.getElementsByClassName("slideshow-container")[0];
 
-logo = document.getElementById("logo").style;
+startBtn.addEventListener("click", function () {
+  startBtn.style.display = "none";
+  container.style.display = "block";
+  showSlides(slideIndex);
+  playAudio();
+});
 
-audio = document.getElementById("audio");
-
-window.onclick = function () {
-  if (!context) {
-    preparation();
-  }
-  if (audio.paused) {
-    audio.play();
-    loop();
-  } else {
-    audio.pause();
-  }
-};
-
-function preparation() {
-  context = new AudioContext();
-  analyser = context.createAnalyser();
-  src = context.createMediaElementSource(audio);
-  src.connect(analyser);
-  analyser.connect(context.destination);
-  loop();
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+  playAudio();
 }
 
-function loop() {
-  if (!audio.paused) {
-    window.requestAnimationFrame(loop);
-  }
-  array = new Uint8Array(analyser.frequencyBinCount);
-  analyser.getByteFrequencyData(array);
+var audios = document.getElementsByClassName("Audio");
+var canvas = document.getElementById("canvas");
 
-  logo.minHeight = array[40] + "px";
-  logo.width = array[40] + "px";
+function playAudio() {
+  for (let i = 0; i < audios.length; i++) {
+    audios[i].pause();
+  }
+  audios[slideIndex - 1].currentTime = 0;
+  // audios[slideIndex - 1].play();
+  visualPlayAudio(audios[slideIndex - 1]);
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace("active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+function visualPlayAudio(audio) {
+  audio.play();
+  var context = new AudioContext();
+  var src = context.createMediaElementSource(audio);
+  var analyser = context.createAnalyser();
+
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  var ctx = canvas.getContext("2d");
+
+  src.connect(analyser);
+  analyser.connect(context.destination);
+
+  analyser.fftSize = 256;
+
+  var bufferLength = analyser.frequencyBinCount;
+  console.log(bufferLength);
+
+  var dataArray = new Uint8Array(bufferLength);
+
+  var WIDTH = canvas.width;
+  var HEIGHT = canvas.height;
+
+  var barWidth = (WIDTH / bufferLength) * 2.5;
+  var barHeight;
+  var x = 0;
+
+  function renderFrame() {
+    requestAnimationFrame(renderFrame);
+
+    x = 0;
+
+    analyser.getByteFrequencyData(dataArray);
+
+    ctx.fillStyle = "#000";
+    ctx.fillRect(0, 0, WIDTH, HEIGHT);
+
+    for (var i = 0; i < bufferLength; i++) {
+      barHeight = dataArray[i];
+
+      var r = barHeight + 25 * (i / bufferLength);
+      var g = 250 * (i / bufferLength);
+      var b = 50;
+
+      ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+      ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
+
+      x += barWidth + 1;
+    }
+  }
+
+  audio.play();
+  renderFrame();
 }
